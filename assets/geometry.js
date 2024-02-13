232e2e2dd06e68e627c8e905167c12dc4d9df67a
ayashhossain555 <ayash.ahmedh555@gmail.com>
2024-02-13 21:35:20 +0600

    updated 4 more geometrical shape are  calculation

--- assets/geometry.js
+++ assets/geometry.js
@@ -1,33 +1,81 @@
 console.log(`Connected`);
 
-function calculateTriangleArea(){
-    const result = document.getElementById('result');
+function showResult(resultId){
+    const result = document.getElementById(resultId);
     result.classList.remove('hidden');
+}
 
-    const triBaseInput = document.getElementById('tri-base').value;
-    const triBase = parseFloat(triBaseInput);
+function getValue(inputId){
+    const inputText = document.getElementById(inputId).value;
+    const inputValue = parseFloat(inputText);
+    return inputValue;
+}
 
-    const triheightInput = document.getElementById('tri-height').value;
-    const triheight = parseFloat(triheightInput);
+function setArea(areaId,area){
+    const inputArea = document.getElementById(areaId);
+    inputArea.innerText = area;
+}
+
+function calculateTriangleArea(){
+    showResult('result');
+
+    const triBase = getValue('tri-base');
+    const triheight = getValue('tri-height');
     
     const area = 0.5 * triBase * triheight;
 
-    const triArea = document.getElementById('tri-area');
-    triArea.innerText = area;
+    setArea('tri-area',area);
 }
 
 function calculateRectangleArea(){
-    const result = document.getElementById('result2');
-    result.classList.remove('hidden');
+    showResult('result2');
 
-    const recWidthInput = document.getElementById('rec-width').value;
-    const recWidth = parseFloat(recWidthInput);
+    const recWidth = getValue('rec-width');
+    const recLength = getValue('rec-length');
 
-    const recLengthInput = document.getElementById('rec-length').value;
-    const recLength = parseFloat(recLengthInput);
-    
     const area = recWidth * recLength;
 
-    const recArea = document.getElementById('rec-area');
-    recArea.innerText = area;
+    setArea('rec-area',area);
+}
+
+function calculateParallelogramArea(){
+    showResult('result3');
+
+    const paraBase = getValue('para-base');
+    const paraHeight = getValue('para-height');
+
+    const area = paraBase * paraHeight;
+
+    setArea('para-area',area);
+}
+
+function calculateRhombusArea(){
+    showResult('result4');
+
+    const rhomD1 = getValue('rhom-d1');
+    const rhomD2 = getValue('rhom-d2');
+
+    const area = 0.5 * rhomD1 * rhomD2;
+
+    setArea('rhom-area',area);
+}
+function calculatePentagonArea(){
+    showResult('result5');
+
+    const penP = getValue('pen-p');
+    const penB = getValue('pen-b');
+
+    const area = 0.5 * penP * penB;
+
+    setArea('pen-area',area);
+}
+function calculateEllipseArea(){
+    showResult('result6');
+
+    const eliA = getValue('eli-a');
+    const eliB = getValue('eli-b');
+
+    const area = Math.PI * eliA * eliB;
+
+    setArea('eli-area',area);
 }
